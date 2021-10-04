cb0f1403fe12869e1ef7a8d4a999ea83c2225a5f
VSAlmeida <vsalmeida@ibm.com>
2021-10-03 21:05:36 -0300

    feat: add logger helper

---
+++ src/helpers/logger.js
@@ -0,0 +1,20 @@
+const chalk = require('chalk');
+
+// eslint-disable-next-line
+const log = console.log;
+
+const info = (msg, color = '#F8F8F2') => {
+  log(chalk.hex(color)(msg));
+};
+
+const success = (msg, color = '#50FA7B') => {
+  log(chalk.hex(color)(msg));
+};
+const warning = (msg, color = '#F1FA8C') => {
+  log(chalk.hex(color)(msg));
+};
+const error = (msg, color = '#FF5555') => {
+  log(chalk.hex(color)(msg));
+};
+
+module.exports = { info, success, warning, error };
