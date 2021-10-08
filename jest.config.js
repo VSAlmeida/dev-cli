9c006da411a144759e452f7680a4bce6341f4bde
VSAlmeida <vsalmeida@ibm.com>
2021-10-08 10:45:45 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const { name } = require('./package.json');
+
+module.exports = {
+  displayName: name,
+  clearMocks: true,
+  verbose: true,
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+};
